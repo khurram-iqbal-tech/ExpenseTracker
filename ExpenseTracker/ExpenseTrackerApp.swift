b756d63c8e5bd3b8b77ff2cbd63cb0105cd12d65
khurram-iqbal-tech <khurram.ms1@gmail.com>
2025-01-27 18:35:15 +0500

    Initial Commit

---
+++ ExpenseTracker/ExpenseTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerApp.swift
+//  ExpenseTracker
+//
+//  Created by MacBook Pro on 27/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct ExpenseTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ExpenseTrackerTests/ExpenseTrackerTests.swift
@@ -0,0 +1,17 @@
+//
+//  ExpenseTrackerTests.swift
+//  ExpenseTrackerTests
+//
+//  Created by MacBook Pro on 27/01/2025.
+//
+
+import Testing
+@testable import ExpenseTracker
+
+struct ExpenseTrackerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
